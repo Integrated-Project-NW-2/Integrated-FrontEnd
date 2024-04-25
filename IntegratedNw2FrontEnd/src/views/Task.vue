4c9dd3ee8e174becbdeda05c41e025f9fdde478c
Tiskas02 <saksit.tati@mail.kmutt.ac.th>
2024-04-25 22:32:10 +0700

    add span page

--- IntegratedNw2FrontEnd/src/views/Task.vue
+++ IntegratedNw2FrontEnd/src/views/Task.vue
@@ -19,32 +19,17 @@ const setDetail = (set) => {
     <div class="justify-center text-center w-full py-3 bg-white">
       <h1 class="font-bold text-2xl text-gray-800">IT-Bangmod Kradan Kanban</h1>
     </div>
-    <div class="overflow-x-auto flex">
-      <div class="border justify-center ">
-      <table class="min-w-full divide-y divide-gray-200 border ">
-        <thead class="bg-gray-50">
-          <tr>
-            <th
-              class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider w-[10%]"
-            ></th>
-            <th
-              class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider w-[30%]"
-            >
-              Title
-            </th>
-            <th
-              class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider w-[30%]"
-            >
-              Assignees
-            </th>
-            <th
-              class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider w-[30%]"
-            >
-              Status
-            </th>
-          </tr>
-        </thead>
-        <tbody class="bg-white divide-y divide-gray-200">
+    <div class="overflow-x-auto">
+  <table class="min-w-full divide-y divide-gray-200">
+    <thead class="bg-gray-50">
+      <tr>
+        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"></th>
+        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Title</th>
+        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Assignees</th>
+        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Status</th>
+      </tr>
+    </thead>
+    <tbody class="bg-white divide-y divide-gray-200">
           <div v-if="taskManagement.getTask().length === 0 " class="">No task</div>
           <tr v-for="task in taskManagement.getTask()" :key="task.taskId" class="itbkk-item">
             <td class="px-6 py-4 whitespace-nowrap">{{ task.taskId }}</td>
@@ -59,9 +44,9 @@ const setDetail = (set) => {
             </td>
           </tr>
         </tbody>
-      </table>
-    </div>
-    </div>
+  </table>
+</div>
+
     <teleport to="body" v-if="showDetail">
     <Modal @setDetail="setDetail"></Modal>
   </teleport>
